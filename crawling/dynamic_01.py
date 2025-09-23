29d427629e4d99ea2747af81ebe2279dd0d30ac0
mminguu <mminguuu7@gmail.com>
2025-09-23 16:33:54 +0900

    dynamic_01 update

--- crawling/dynamic_01.py
+++ crawling/dynamic_01.py
@@ -4,19 +4,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys # enter키 등을 입력하기 위해서 
+from selenium.webdriver.common.keys import Keys # enter, space 같은 키를 누르게 할 때 필요해서 
 
 # 웹 드라이버를 자동으로 설치하고 최신버전을 유지
 Service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=Service)
 
+
+
 driver.get('https://www.naver.com/')
+time.sleep(5)
 print('브라우저가 성공적으로 열렸습니다.')
 
 # 검색창 요소 찾기 ( id 가 ipt_keyword_recruit 인 input 태그를 찾음 )
-search_input = driver.find_element(By.ID,'ipt_keyword_recruit')
+search_input = driver.find_element(By.ID,'query')
 # 검색창에 파이썬을 입력
-search_input.send_keys('파이썬')
-search_input.send_keys(Keys.ENTER)
-time.sleep(10)
+search_input.send_keys('파이썬') # send_keys->글자를 입력하게 하는 코드
+search_input.send_keys(Keys.ENTER) # enter키를 눌러 검색을 실행하게 함
+time.sleep(5)
 driver.quit()
